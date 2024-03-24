95236b972e0dac5ea67d8983a399875f4a775d73
nashgithub1 <136361416+nashgithub1@users.noreply.github.com>
2024-03-24 11:23:54 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
+
 def fibonacci_generator(n):
-    # Initialize variables for the first two Fibonacci numbers
+    # Allocate variables for the first two Fibonacci numbers
     a, b = 0, 1
 
     # Generate Fibonacci sequence up to the nth number
@@ -7,17 +9,23 @@ def fibonacci_generator(n):
         yield a
         a, b = b, a + b
 
+def instructions():
+    print("Instructions")
+    print("**Cannot Generate Fibonacci for Fractional, Decimal, Real or Complex Numbers")
+    print("**Cannot Generate Fibonacci for Negative values")
+    print("**Cannot Generate Fibonacci for Zero")
+    print("**Cannot Generate Fibonacci for Non-Numerical values\n")
 
-def main():
-    print("Welcome to the Fibonacci Number Generator!")
+
+def interface():
     while True:
         try:
-            n = int(input("Enter the number of Fibonacci numbers to generate (0 to exit): "))
+            n = int(input("Enter the number of Fibonacci numbers to generate (0 to main menu): "))
             if n < 0:
                 print("Please enter a non-negative integer.")
                 continue
             elif n == 0:
-                print("Exiting the program. Goodbye!")
+                print("Exiting the calculator...")
                 break
             else:
                 # Generate Fibonacci numbers using the generator
@@ -25,10 +33,40 @@ def main():
                 fibonacci_numbers = list(fib_gen)
 
                 # Print the generated Fibonacci numbers
-                print("Generated Fibonacci numbers:", fibonacci_numbers)
+                print("Generated Fibonacci numbers:", fibonacci_numbers,"\n")
         except ValueError:
             print("Invalid input. Please enter a valid integer.")
+
+def info():
+    print("Developed by")
+    print("Mohamed Nashath")
+    print("nashathmjm1@gmail.com")
+    print("https://www.linkedin.com/in/mohamed-nashath-27a9142b6/")
+    print("A mini project for CodeAlpha internship programme")
+
+
+def main():
+    while True:
+        print("*** Welcome to the Fibonacci Number Generator mini app ***")
+        print("Press 1 for Instructions")
+        print("Press 2 for Fibonacci Calculator")
+        print("Press 3 for Developer Info")
+        print("Press 0 for Exit")
+        try:
+            x = int(input())
+            if x == 1:
+                instructions()
+            elif x == 2:
+                interface()
+            elif x == 3:
+                info()
+            elif x == 0:
+                print("Exiting the program... Goodbye...")
+                break
+        except ValueError:
+                print("Invalid option. Please choose options from 1 to 3 or 0.")
 
 
 if __name__ == "__main__":
     main()
+
